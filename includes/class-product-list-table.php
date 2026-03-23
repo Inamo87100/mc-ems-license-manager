46bfa71f191174e9b173689f013e5555fd43a994
Inamo87100 <internetamodo@gmail.com>
2026-03-23 15:18:33 +0100

    Update column_actions method to keep only the Delete button

--- includes/class-product-list-table.php
+++ includes/class-product-list-table.php
@@ -1,12 +1,23 @@
-public function column_actions( $item ) {
-    $delete_nonce = wp_create_nonce( 'delete_product' );
-    $delete_link = sprintf( "<a href='?page=%s&action=delete&product_id=%d&_wpnonce=%s' title='%s'>%s</a>",
-        esc_attr( 'your_page_slug' ),
-        $item['product_id'],
-        $delete_nonce,
-        esc_attr( 'Delete this item' ),
-        __('Delete', 'your-text-domain')
-    );
+<?php
+// Assuming this is the structure of the file
 
-    return $delete_link;
+class Product_List_Table extends WP_List_Table {
+    
+    // Other methods
+
+    function column_actions( $item ) {
+        $actions = array();
+
+        // Keeping only the Delete button
+        $actions['delete'] = sprintf( '<a href="%s">%s</a>',
+            wp_nonce_url( admin_url( "admin.php?page=your_page&action=delete&product_id={$item['id']}" ), 'delete_product' ),
+            __( 'Delete', 'your-text-domain' )
+        );
+
+        return $actions;
+    }
+
+    // Other methods
 }
+
+?>
